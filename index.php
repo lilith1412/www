001e2ed76d2aca443857874b6426447d424ef85e
Cesar Orrego A <orrego.cesar@gmail.com>
2022-05-05 18:04:15 -0500

    commit inicial

---
+++ index.php
@@ -0,0 +1,52 @@
+
+       <?php
+       include 'header.php';
+       echo $header_html;
+          $car_brands = array("Volvo" => array("country" => "Germany", "since" => "1956"),
+                              "Mercedes" => array("country" => "Germany", "since" => "1945"),
+                              "McLaren" => array("country" => "Italy", "since" => "1942"),
+                              "Lada" => array("country" => "Russia", "since" => "1975"));
+        ?>
+
+        <table class="table">
+          <thead>
+            <tr>
+              <td>Marca</td>
+              <td>Procedencia</td>
+              <td>Desde</td>
+            </tr>
+          </thead>
+        <tbody>
+          <?php
+          if(array_key_exists('brand_label', $_REQUEST) && array_key_exists($_REQUEST['brand_label'], $car_brands)){
+            $brand_data = $car_brands[$_REQUEST['brand_label']];
+            echo '<tr>';
+              echo '<td>';
+                echo $_REQUEST['brand_label'];
+              echo '</td>';
+              echo '<td>';
+                echo $brand_data['country'];
+              echo '</td>';
+              echo '<td>';
+                echo $brand_data['since'];
+              echo '</td>';
+            echo '</tr>';
+          }else{
+          foreach ($car_brands as $brand_label => $brand_data) {
+          echo '<tr>';
+            echo '<td>';
+              echo $brand_label;
+            echo '</td>';
+            echo '<td>';
+              echo $brand_data['country'];
+            echo '</td>';
+            echo '<td>';
+              echo $brand_data['since'];
+            echo '</td>';
+          echo '</tr>';
+          }
+        }
+          ?>
+        </tbody>
+        </table>
+        <?php echo $footer_html; ?>
---
+++ welcome_get.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>SIN - 2022-I</title>
+    <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
+<body>
+  <?php
+    echo date("Y-m-d H:i:s");
+   ?>
+   <br>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
