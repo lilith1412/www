758a7132919e4d14eccc8d9e8a8620eba22d8c48
Cesar Orrego A <orrego.cesar@gmail.com>
2022-06-07 18:45:26 -0500

    se implementan validaciones en servidor y cliente con feedback para el usuario

--- customers/create.php
+++ customers/create.php
@@ -1,7 +1,52 @@
 <?php
+session_start();
 $con = mysqli_connect("db", "root", "root", "northwind");
-$sql = "INSERT INTO customers (CustomerID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country) VALUES ('".$_POST['CustomerID']."', '".$_POST['CompanyName']."', '".$_POST['ContactName']."', '".$_POST['ContactTitle']."', '".$_POST['Address']."', '".$_POST['City']."', '".$_POST['Region']."', '".$_POST['PostalCode']."', '".$_POST['Country']."')";
-$result = mysqli_query($con, $sql);
-header("location: index.php")
+
+$customer_id = $_POST['CustomerID'];
+$company_name = $_POST['CompanyName'];
+$contact_name = $_POST['ContactName'];
+$contact_title = $_POST['ContactTitle'];
+$address = $_POST['Address'];
+$city = $_POST['City'];
+$region = $_POST['Region'];
+$postal_code = $_POST['PostalCode'];
+$country = $_POST['Country'];
+
+$errors = array();
+function validate_customer_id(){
+
+  global $con, $customer_id, $errors;
+  if (trim($customer_id) == ''){
+    $errors["CustomerID"] = "CustomerID no puede ser vacío";
+  }else{
+    $sql_uniqness_validation = "SELECT * from customers where CustomerID = '".$customer_id."'";
+    $result = mysqli_query($con, $sql_uniqness_validation);
+    if (mysqli_num_rows($result) > 0){
+      $errors["CustomerID"] = "CustomerID no puede repetirse";
+    }
+  }
+}
+
+validate_customer_id();
+$_SESSION["errors"] = $errors;
+if(count($errors) == 0){
+  $sql = "INSERT INTO customers (CustomerID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country) VALUES ('".$customer_id."', '".$company_name."', '".$contact_name."', '".$contact_title."', '".$address."', '".$city."', '".$region."', '".$postal_code."', '".$country."')";
+  $result = mysqli_query($con, $sql);
+  header("location: index.php");
+}else{
+  $_SESSION["values"]['CustomerID'] = $customer_id;
+  $_SESSION["values"]['CompanyName'] = $company_name;
+  $_SESSION["values"]['ContactName'] = $contact_name;
+  $_SESSION["values"]['ContactTitle'] = $contact_title;
+  $_SESSION["values"]['Address'] = $address;
+  $_SESSION["values"]['City'] = $city;
+  $_SESSION["values"]['Region'] = $region;
+  $_SESSION["values"]['PostalCode'] = $postal_code;
+  $_SESSION["values"]['Country'] = $country;
+  header("location: new.php");
+}
+
+
+
 
  ?>
--- customers/new.php
+++ customers/new.php
@@ -1,6 +1,9 @@
 <?php
   include '../header.php';
   echo $header_html;
+  if(!isset($_SESSION["errors"])){
+    unset($_SESSION["values"]);
+  }
  ?>
  <div class="container-fluid p-5 bg-primary text-white text-center">
    <h1>Crear un nuevo cliente</h1>
@@ -21,64 +24,70 @@
        <div class="row">
          <div class="col-sm-2">ID</div>
          <div class="col-sm-8">
-           <input type="text" id="CustomerID" name="CustomerID">
+           <input type="text" class="form-control <?php echo isset($_SESSION["values"]["CustomerID"]) ? 'is-invalid' : '';  ?>"  value="<?php echo isset($_SESSION["values"]["CustomerID"]) ? $_SESSION["values"]["CustomerID"] : '';  ?>" maxlength="5" id="CustomerID" name="CustomerID">
          </div>
+         <div class="col-sm-2"><?php echo isset($_SESSION["errors"]["CustomerID"]) ? $_SESSION["errors"]["CustomerID"] : '';  ?></div>
        </div>
        <div class="row">
          <div class="col-sm-2">Nombre</div>
          <div class="col-sm-8">
-           <input type="text" id="CompanyName" name="CompanyName">
+           <input type="text" class="form-control"  value="<?php echo isset($_SESSION["values"]["CompanyName"]) ? $_SESSION["values"]["CompanyName"] : '';  ?>" id="CompanyName" name="CompanyName" required>
          </div>
        </div>
        <div class="row">
          <div class="col-sm-2">Contacto</div>
          <div class="col-sm-8">
-           <input type="text" id="ContactName" name="ContactName">
+           <input type="text" class="form-control"  value="<?php echo isset($_SESSION["values"]["ContactName"]) ? $_SESSION["values"]["ContactName"] : '';  ?>" id="ContactName" name="ContactName">
          </div>
        </div>
        <div class="row">
          <div class="col-sm-2">Titulo Contacto</div>
          <div class="col-sm-8">
-           <input type="text" id="ContactTitle" name="ContactTitle">
+           <input type="text" class="form-control"  value="<?php echo isset($_SESSION["values"]["ContactTitle"]) ? $_SESSION["values"]["ContactTitle"] : '';  ?>" id="ContactTitle" name="ContactTitle">
          </div>
        </div>
        <div class="row">
          <div class="col-sm-2">Dirección</div>
          <div class="col-sm-8">
-           <input type="text" id="Address" name="Address">
+           <input type="text" class="form-control"  value="<?php echo isset($_SESSION["values"]["Address"]) ? $_SESSION["values"]["Address"] : '';  ?>" id="Address" name="Address">
          </div>
        </div>
        <div class="row">
          <div class="col-sm-2">Ciudad</div>
          <div class="col-sm-8">
-           <input type="text" id="City" name="City">
+           <input type="text" class="form-control"  value="<?php echo isset($_SESSION["values"]["City"]) ? $_SESSION["values"]["City"] : '';  ?>" id="City" name="City">
          </div>
        </div>
        <div class="row">
          <div class="col-sm-2">Región</div>
          <div class="col-sm-8">
-           <input type="text" id="Region" name="Region">
+           <input type="text" class="form-control" value="<?php echo isset($_SESSION["values"]["Region"]) ? $_SESSION["values"]["Region"] : '';  ?>" id="Region" name="Region">
          </div>
        </div>
        <div class="row">
          <div class="col-sm-2">Código postal</div>
          <div class="col-sm-8">
-           <input type="text" id="PostalCode" name="PostalCode">
+           <input type="text" class="form-control"  value="<?php echo isset($_SESSION["values"]["PostalCode"]) ? $_SESSION["values"]["PostalCode"] : '';  ?>" id="PostalCode" name="PostalCode">
          </div>
        </div>
        <div class="row">
          <div class="col-sm-2">País</div>
          <div class="col-sm-8">
-           <input type="text" id="Country" name="Country">
+           <input type="text" class="form-control"  value="<?php echo isset($_SESSION["values"]["Country"]) ? $_SESSION["values"]["Country"] : '';  ?>" id="Country" name="Country">
          </div>
        </div>
        <div class="row">
          <div class="col-sm-2">&nbsp;</div>
          <div class="col-sm-8">
-           <input type="submit">
+           <input type="submit" class="btn btn-primary" >
          </div>
        </div>
      </div>
    </div>
  </div>
 </form>
+
+<?php
+unset($_SESSION["errors"]);
+unset($_SESSION["values"]);
+?>
